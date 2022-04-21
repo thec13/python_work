492f1dc65254ca13ff0d853573e82d385ce66e8f
Chad Ramey <thec13@gmail.com>
2022-04-20 20:34:00 -0700

    SyntaxError: invalid syntax

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# SyntaxError: invalid syntax
+message = 'One of Python's strengths is its diverse community.'
+print(message)
