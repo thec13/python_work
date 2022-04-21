bf19c2b28c34afc23cf4b58e38c38a57742e102b
Chad Ramey <thec13@gmail.com>
2022-04-20 20:24:12 -0700

    Space Tabs Newlines strips

---
+++ space_tabs_newlines.py
@@ -0,0 +1,30 @@
+# tabs
+print("Python")
+print("\tPython")
+
+# newlines
+print("Languages:\nPython\nC\nJavaScript")
+
+##
+# stripping whitespace
+# >>> favorite_language = 'python '
+# >>> favorite_language
+# 'python '
+# >>> favorite_language.rstrip()
+# 'python'
+# >>> favorite_language
+# 'python '
+##
+# >>> favorite_language = 'python '
+# >>> favorite_language = favorite_language.rstrip()
+# >>> favorite_language
+# 'python'
+##
+# >>> favorite_language = ' python '
+# >>> favorite_language.rstrip()
+# ' python'
+# >>> favorite_language.lstrip()
+# 'python '
+# >>> favorite_language.strip()
+# 'python'
+##
